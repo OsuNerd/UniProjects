c17310d930eeed38d7644378fd03f16acd43ab7d
OsuNerd <90028612+OsuNerd@users.noreply.github.com>
2021-11-01 12:09:39 +0200

    Update Lab_3.py

--- Labs/Lab_3.py
+++ Labs/Lab_3.py
@@ -1,24 +1,49 @@
-# 3 Integer numbers input; Operator %
+# 3 Integer numbers input; All
 integer_numbers = []
 print("Input some integer numbers")
 for i in range(1, 4):
     integer_numbers.append(int(input(f'Integer number #{i}: ')))
-# Output
-for i, a in zip(integer_numbers, range(1, 4)):
-    print('Result #{}:'.format(a), '%4.0f' % i)
 
-# 3 Real numbers input; str.format() and f'
+# Output
+# % Operator
+for i, a in zip(integer_numbers, range(1, 4)):
+    print('% Result #{}:'.format(a), '%4d' % i)
+# str.format()
+for i, a in zip(integer_numbers, range(1,4)):
+    print('str.format Result #{}:'.format(a), '{0:4}'.format(i))
+# f'
+for i, a in zip(integer_numbers, range(1, 4)):
+    print("f' Result #{}:".format(a), f'{i:4}')
+
+
+# 3 Real numbers input; All
 real_numbers = []
 print("Input some real numbers")
 for i in range(1, 4):
     real_numbers.append(float(input('Real number #{}: '.format(i))))
-# Output
+
+# Floating Output
+# % Operator
+for i, a in zip(real_numbers, range(1, 4)):
+    print('% Floating point result #{}:'.format(a), '%9s' % i)
 # str.format()
 for i, a in zip(real_numbers, range(1, 4)):
-    print('Floating point result #{}'.format(a), '{0:9}'.format(i))
+    print('Floating point result #{}:'.format(a), '{0:9}'.format(i))
 # f'
 for i, a in zip(real_numbers, range(1, 4)):
-    print('Fixed point result #{}'.format(a), f'{i:6.3f}')
+    print("f' Floating point result #{}:".format(a), f'{i:9}')
+
+# Fixed poing Output
+# % Operator
+for i, a in zip(real_numbers, range(1, 4)):
+    print('Fixed point result #{}:'.format(a), '%6.3f' % i)
+# str.format()
+for i, a in zip(real_numbers, range(1, 4)):
+    print('Fixed point result #{}:'.format(a), '{0:6.3f}'.format(i))
+# f'
+for i, a in zip(real_numbers, range(1, 4)):
+    print('Fixed point result #{}:'.format(a), f'{i:6.3f}')
+
 
 # 3 string symols in a line;
 string_value = input("Input any symbols (string): ")
@@ -30,4 +55,5 @@ for i in range(0, int(-(-counter//3))):
 
 # Boolean
 print(bool(0))
+# or
 print(bool())
